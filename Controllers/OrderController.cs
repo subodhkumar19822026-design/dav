d23732f294d5c78799473c5bfdd31842bb440eb2
Subodh Kumar <subodh.kumar19822026@gmail.com>
2026-06-13 20:03:15 +0530

    Application Updated

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dav.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
